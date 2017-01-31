3240b69f34f55c834848883823e5a0aab4a1ed16
Quentin de Longraye <quentin@dldl.fr>
2017-01-31 21:48:49 +0100

    add ability to preserve words from being cut

--- src/string/truncate.js
+++ src/string/truncate.js
@@ -1,16 +1,23 @@
 /**
  * truncate text to a specified length.
  *
- * {{ 'this is a big city!' | truncate 10 '...' }} => this is...
+ * {{ 'this is a big city!' | truncate 11 '...' true }} => this is...
  */
 
-function truncate(str, length, ellipses) {
+function truncate(str, length, ellipses, preserveWords) {
     length = length || 30;
-    if(ellipses === undefined){
+    str = (str.length > length) ? str.slice(0, length) : str;
+
+    if (preserveWords) {
+        str = str.substr(0, Math.min(str.length, str.lastIndexOf(' ')));
+    }
+
+    if (ellipses === undefined) {
         ellipses = '...';
     }
     ellipses = '' + ellipses;
-    return (str.length > length ? str.slice(0, length - ellipses.length) + ellipses : str);
+
+    return str + ellipses;
 }
 
 export default truncate;
