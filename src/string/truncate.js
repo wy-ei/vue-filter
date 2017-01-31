0848984e6d8a1231ec52687211fa3d8b3079d1b3
Quentin de Longraye <quentin@dldl.fr>
2017-01-31 21:57:57 +0100

    fix ellipses

--- src/string/truncate.js
+++ src/string/truncate.js
@@ -1,21 +1,22 @@
 /**
  * truncate text to a specified length.
  *
- * {{ 'this is a big city!' | truncate 11 '...' true }} => this is...
+ * {{ 'this is a big city!' | truncate 15 '...' true }} => this is a...
  */
 
 function truncate(str, length, ellipses, preserveWords) {
     length = length || 30;
-    str = (str.length > length) ? str.slice(0, length) : str;
-
-    if (preserveWords) {
-        str = str.substr(0, Math.min(str.length, str.lastIndexOf(' ')));
-    }
 
     if (ellipses === undefined) {
         ellipses = '...';
     }
     ellipses = '' + ellipses;
+
+    str = (str.length > length) ? str.slice(0, length - ellipses.length) : str;
+
+    if (preserveWords) {
+        str = str.substr(0, Math.min(str.length, str.lastIndexOf(' ')));
+    }
 
     return str + ellipses;
 }
